10c9f6afc014e499cdec3fc7d1ee39ac568c655c
Nate Buel <natebuel29@gmail.com>
2022-05-19 20:08:06 -0500

    initial design of app and setup log reg that that returns results for a given date (this is extremely rough and will be redesigned)

--- ufc_predictor/app_factory.py
+++ ufc_predictor/app_factory.py
@@ -4,5 +4,11 @@ from flask import Flask
 def create_app(config_object):
     app = Flask(__name__)
     app.config.from_object(config_object)
-    print("yo we here son")
+
+    from .logistic_regression import views
+    app.register_blueprint(views.logistic_regresion_views)
+
+    @app.route('/hello')
+    def hello():
+        return 'Hello World!'
     return app
