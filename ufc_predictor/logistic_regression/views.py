7d75376d0601832d6f7f5800452637936125076c
Nate Buel <natebuel29@gmail.com>
2022-07-02 20:29:56 -0500

    log the date and display last saturdays event on monday

--- ufc_predictor/logistic_regression/views.py
+++ ufc_predictor/logistic_regression/views.py
@@ -12,14 +12,14 @@ logistic_regresion_views = Blueprint('logistic_regression', __name__)
 @logistic_regresion_views.route('/')
 def logistic_regression():
     saturday_date = ""
+    print(pendulum.now().weekday())
     if pendulum.now().weekday() != 5:
         saturday_date = pendulum.now().next(pendulum.SATURDAY).format('YYYY-MM-DD')
-    elif pendulum.now().weekday() == 6:
+    elif pendulum.now().weekday() == 6 or pendulum.now().weekday() == 0:
         saturday_date = pendulum.now().previous(pendulum.SATURDAY).format('YYYY-MM-DD')
     else:
         saturday_date = pendulum.now().format('YYYY-MM-DD')
 
-    print(saturday_date)
     results, r_fighters, b_fighters, event_name = logistic_regression_service.predict(
         saturday_date)
     results_rf_bf = zip(results, r_fighters, b_fighters)
