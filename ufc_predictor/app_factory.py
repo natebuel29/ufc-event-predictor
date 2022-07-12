9b48f4dc78ca0084fca50e04e05870cc31b8fba1
Nate Buel <natebuel29@gmail.com>
2022-07-11 23:17:46 -0500

    add logging config

--- ufc_predictor/app_factory.py
+++ ufc_predictor/app_factory.py
@@ -1,13 +1,29 @@
 from flask import Flask
 from ufc_predictor import db
-import logging
+from logging.config import dictConfig
 
 
 def create_app(config_object):
-    logging.info(
-        f"Creating ufc-event-predictor app for {config_object.ENVIRONMENT} env")
+    # config for the logger
+    dictConfig({
+        'version': 1,
+        'formatters': {'default': {
+            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+        }},
+        'handlers': {'wsgi': {
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://flask.logging.wsgi_errors_stream',
+            'formatter': 'default'
+        }},
+        'root': {
+            'level': 'INFO',
+            'handlers': ['wsgi']
+        }
+    })
+
     app = Flask(__name__)
-    app.logger.setLevel(logging.INFO)
+    app.logger.info(
+        f"Creating ufc-event-predictor app for {config_object.ENVIRONMENT} env")
     app.config.from_object(config_object)
     db.mysql.init_app(app)
     # TODO: delete these two functions after the db migration
